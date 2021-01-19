7d4276ee41f1be70ec413a378811728294cc22c8
kraken808 <murat99.04@gmail.com>
2021-01-19 15:38:14 +0600

    pagination + stackView for small views

--- ChocofoodProject/ViewCells/SmallMenuCell.swift
+++ ChocofoodProject/ViewCells/SmallMenuCell.swift
@@ -25,14 +25,28 @@ class SmallMenuCell: UICollectionViewCell{
     //
         func bindData(data: Cafe){
             restaurantName.text = data.restaurant.title
-    
+            imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight, .flexibleBottomMargin, .flexibleRightMargin, .flexibleLeftMargin, .flexibleTopMargin]
+            imageView.contentMode = .scaleAspectFill // OR .scaleAspectFill
+            imageView.clipsToBounds = true
             imageView.sd_setImage(with: URL(string: data.restaurant.image), completed: nil)
+            
             let cost = data.delivery_tariff.conditions[0].order_min_cost
             cashTenge.text = "\(cost.formattedWithSeparator) тг"
-            let rate = Double(data.restaurant.rating)
-            rating.text = "\(rate*5/100)"
+            let rate = Double(data.restaurant.rating)*5/100
+            let rateStr = String(format: "%.1f", rate)
+            rating.text = rateStr
             deliveryCash.text = "от \(data.delivery_tariff.conditions[0].delivery_cost) тг"
     
             deliveryTime.text = "\(data.delivery_time.low_limit_minutes) мин."
         }
+    func contentMode(image: UIImage?)->UIView.ContentMode{
+        guard let image = image else { return .scaleAspectFill }
+          let viewAspectRatio = imageView.bounds.width / imageView.bounds.height
+          let imageAspectRatio = image.size.width / image.size.height
+          if viewAspectRatio > imageAspectRatio {
+             return .scaleAspectFill
+          } else {
+              return .scaleAspectFit
+          }
+    }
 }
